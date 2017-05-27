462f9e5ba35dabbae3c8885b69e6bb046e48c14b
Natasha Gitlin <natasha.gitlin8@gmail.com>
2017-05-27 13:04:33 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,21 @@
-class Student 
+class Student < User
 
+  attr_accessor :knowledge
+
+  def initialize
+    @knowledge = []
+  end
+  #initializes with an empty knowledge array
+
+  def learn(knowledge)
+    @knowledge << knowledge
+  end
+  #takes in an argument of a string of knowledge and adds it
+  #to the student's knowledge array
+
+  def knowledge
+    @knowledge
+  end
+  #returns that student's knowledge array
+  
 end
